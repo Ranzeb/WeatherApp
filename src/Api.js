bcca295b0c0c6634dfa3aa5f40a259a3d8270a90
Ranzeb <r.ranzieri@gmail.com>
2022-08-12 15:45:58 +0200

    First Working Version
    Look for a city and retrieve current weather and next 5 days weather.

---
+++ src/Api.js
@@ -0,0 +1,14 @@
+import config
+ from "../config";
+export const geoAPIOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': config.RAPID_API_KEY,
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = config.WEATHER_API_KEY;
